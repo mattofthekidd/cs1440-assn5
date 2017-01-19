6169fbd1119f67bccd88f44dbaffb62dac83e1e8
Stephen Clyde <swc@mdsc.com>
2017-01-18 23:58:18 -0700

    Added starting point for AnalystComparer, the starting point for HW2

---
+++ AnalystComparer/cmake-build-debug/CMakeFiles/AnalystComparer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AnalystComparer.dir/main.cpp.o"
+  "CMakeFiles/AnalystComparer.dir/Utils.cpp.o"
+  "AnalystComparer.pdb"
+  "AnalystComparer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnalystComparer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AnalystComparer/main.cpp
@@ -0,0 +1,9 @@
+int main(int argv, char* argc[])
+{
+    // Keep the main function simple.  It's responsibility should be limited to handle
+    // the program argument, creating the initial object(s), stimulating them to do
+    // whatever they need to do.
+
+    // For example, think about defining a Comparer class, creating an instance of that
+    // class here, and then have it load the analyst data and doing the comparison.
+}
